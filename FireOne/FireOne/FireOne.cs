2796f0db8b623fb998dd19ce141d261f81b152df
Fabian Williams <fabianwilliams@Fabians-MBP.fios-router.home>
2020-03-21 17:26:09 -0400

    Two examples of Durable Functions now and second has more capabilities with Post and writing to a Blob Container

---
+++ FireOne/FireOne/FireOne.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FireOne
+{
+    public static class FireOne
+    {
+        [FunctionName("FireOne")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("FireOne_Alpha", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("FireOne_Bravo", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("FireOne_Charlie", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("FireOne_Alpha")]
+        public static string SayHelloAlpha([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Logger: Alpha says hello to {name}.");
+            return $"Alpha says hello to {name}!";
+        }
+
+        [FunctionName("FireOne_Bravo")]
+        public static string SayHelloBravo([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Logger: Bravo says hello to {name}.");
+            return $"Bravo says hello to {name}!";
+        }
+
+        [FunctionName("FireOne_Charlie")]
+        public static string SayHelloCharlie([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Logger: Charlie says hello to {name}.");
+            return $"Charlie says hello to {name}!";
+        }
+
+        [FunctionName("FireOne_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("FireOne", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
---
+++ FireOne/FireTwo/FireTwo.cs
@@ -0,0 +1,102 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FireTwo
+{
+    public static class BackupSiteContent
+    {
+        [FunctionName("FireTwo_BackupSiteContent")]
+        public static async Task<long> Run(
+            [OrchestrationTrigger] DurableOrchestrationContext backupContext)
+        {
+            string rootDirectory = backupContext.GetInput<string>()?.Trim();
+            if (string.IsNullOrEmpty(rootDirectory))
+            {
+                rootDirectory = Directory.GetParent(typeof(BackupSiteContent).Assembly.Location).FullName;
+            }
+
+            string[] files = await backupContext.CallActivityAsync<string[]>(
+                "FireTwo_GetFileList",
+                rootDirectory);
+
+            var tasks = new Task<long>[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                tasks[i] = backupContext.CallActivityAsync<long>(
+                    "FireTwo_CopyFileToBlob",
+                    files[i]);
+            }
+
+            await Task.WhenAll(tasks);
+
+            long totalBytes = tasks.Sum(t => t.Result);
+            return totalBytes;
+        }
+
+        [FunctionName("FireTwo_GetFileList")]
+        public static string[] GetFileList(
+            [ActivityTrigger] string rootDirectory,
+            ILogger log)
+        {
+            log.LogInformation($"Searching for files under '{rootDirectory}'...");
+            string[] files = Directory.GetFiles(rootDirectory, "*", SearchOption.AllDirectories);
+            log.LogInformation($"Found {files.Length} file(s) under {rootDirectory}.");
+
+            return files;
+        }
+
+        [FunctionName("FireTwo_CopyFileToBlob")]
+        public static async Task<long> CopyFileToBlob(
+            [ActivityTrigger] string filePath,
+            Binder binder,
+            ILogger log)
+        {
+            long byteCount = new FileInfo(filePath).Length;
+
+            // strip the drive letter prefix and convert to forward slashes
+            string blobPath = filePath
+                .Substring(Path.GetPathRoot(filePath).Length)
+                .Replace('\\', '/');
+            string outputLocation = $"backups/{blobPath}";
+
+            log.LogInformation($"Copying '{filePath}' to '{outputLocation}'. Total bytes = {byteCount}.");
+
+            // copy the file contents into a blob
+            using (Stream source = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream destination = await binder.BindAsync<CloudBlobStream>(
+                new BlobAttribute(outputLocation, FileAccess.Write)))
+            {
+                await Task.Delay(5000); //for demo purposes only so we can see the funcion while its running as it runs really fast
+                await source.CopyToAsync(destination);
+            }
+
+            return byteCount;
+        }
+
+        [FunctionName("FireTwo_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Function, "post")]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            ILogger log)
+                {
+                    // Function input comes from the request content.
+                    string instanceId = await starter.StartNewAsync("FireTwo_BackupSiteContent", null);
+
+                    log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+                    var jsonContent = await req.Content.ReadAsStringAsync();
+                    log.LogInformation($"jsonContent is:  {jsonContent}");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+                }
+    }
+}
